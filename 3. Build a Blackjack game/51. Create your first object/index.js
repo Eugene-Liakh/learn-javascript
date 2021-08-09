e259d48d11d66e0f0ef75db174750c7dd1ee14d9
Jenia <jeka.lyah1992@@gmail.com>
2021-08-09 16:04:06 +0300

    Just doin' it =)

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,11 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+castleHouse = {
+  isCastle: true,
+  location: "Italy",
+  price: 129,
+  premises: ["balcony", "meadow", "lake", "chained_bridge"],
+};
 
+console.log(castleHouse.premises);
